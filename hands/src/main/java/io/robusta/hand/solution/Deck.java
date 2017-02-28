212eaa78a0cb57a6d0793c39ccae67e093d227af
cleymobil <clement.coutey@hotmail.fr>
2017-02-28 10:07:16 +0100

    New tests working

--- hands/src/main/java/io/robusta/hand/solution/Deck.java
+++ hands/src/main/java/io/robusta/hand/solution/Deck.java
@@ -1,45 +1,52 @@
 package io.robusta.hand.solution;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Random;
 import java.util.TreeSet;
 
 import io.robusta.hand.Card;
-import io.robusta.hand.CardColor;
 import io.robusta.hand.interfaces.IDeck;
 
-public class Deck extends LinkedList<Card> implements IDeck{
+public class Deck extends LinkedList<Card> implements IDeck {
 
-	
 	private static final long serialVersionUID = -4686285366508321800L;
-	
+
 	public Deck() {
 
 	}
-	
+
 	@Override
 	public Card pick() {
 		// shuffle;
 		// remove card from deck and returns it
-		return null;
+
+		Collections.shuffle(this);
+		Card card = this.getFirst();
+		this.remove(this.getFirst());
+
+		return card;
 	}
-
-
-	
 
 	@Override
 	public TreeSet<Card> pick(int number) {
+		TreeSet<Card> cards = new TreeSet<Card>();
+		for (int i = 0; i < number; i++) {
+			cards.add(this.pick());
+		}
 		// reuse pick()
-		return null;
+		return cards;
 	}
 
 	@Override
 	public Hand giveHand() {
+		Hand hand = new Hand();
+		for (int i = 0; i < 5; i++) {
+			hand.add(this.pick());
+		}
+
 		// A hand is a **5** card TreeSet
-		return null;
+		return hand;
 	}
-	
-	
-	
+
 }
